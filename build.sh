ea84bd6139256046db4f4bb23fc0a878b8fe603e
Craig Lorentzen <crlorentzen@gmail.com>
2026-01-31 13:26:33 -0500

    modularlize and use local deb when available

--- build.sh
+++ build.sh
@@ -1,47 +1,74 @@
 #!/bin/bash
-
+set -o errexit
 set -o nounset
 set -o pipefail
 set -x
 
-EMBY_VERSION="${1}"
+get_emby() {
+  local EMBY_VERSION=$1
+  local BASE_URL="https://github.com/MediaBrowser/Emby.Releases/releases/download/${EMBY_VERSION}"
+  local EMBY_DEB="emby-server-deb_${EMBY_VERSION}_"
+  
+  ARCH="$(uname -m)"
 
-if [ -z ${EMBY_VERSION} ]; then
-  echo "No Emby Version Provided"
-  exit 1
-fi
+  # Download the correct Emby version for this architecture
+  echo "Detected architecture: $(uname -m)"
+  case "${ARCH}" in
+    x86_64)
+    EMBY_DEB+="amd64.deb"
+    ;;
+  aarch64)
+    EMBY_DEB+="arm64.deb"
+    ;;
+  *)
+    echo "Unknown architecture. Please download and and build with a local deb file https://github.com/MediaBrowser/Emby.Releases/releases"
+    exit 1
+    ;;
+  esac
 
-# Download the correct Emby version for this architecture
-echo "Detected architecture: $(uname -m)"
-if [ "${LINK:-}" ]; then
-  echo "Overriding Emby release"
-  LINK=$LINK
-elif [ "$(uname -m)" == "x86_64" ]; then
-  LINK="https://github.com/MediaBrowser/Emby.Releases/releases/download/${EMBY_VERSION}/emby-server-deb_${EMBY_VERSION}_amd64.deb"
-elif [ "$(uname -m)" == "aarch64" ]; then
-  LINK="https://github.com/MediaBrowser/Emby.Releases/releases/download/${EMBY_VERSION}/emby-server-deb_${EMBY_VERSION}_arm64.deb"
+  local URL="${BASE_URL}/${EMBY_DEB}"
+  echo "Downloading Emby from ${URL}"
+  curl -L -o /tmp/${EMBY_DEB} ${URL}
+}
+
+install_ffmpeg() {
+  local EMBY_DEB=$1
+
+  echo "Installing ffmpeg components from ${EMBY_DEB}"
+
+  ar x ${EMBY_DEB} data.tar.xz
+  tar xf data.tar.xz
+
+  # Put the ffmpeg binaries in the right place, ignoring missing files
+  mv opt/emby-server/bin/ffmpeg /usr/bin/ffmpeg
+  mv opt/emby-server/lib/libav*.so.* /usr/lib/ || true
+  mv opt/emby-server/lib/libpostproc.so.* /usr/lib/ || true
+  mv opt/emby-server/lib/libsw* /usr/lib/ || true
+  mv opt/emby-server/extra/lib/libva*.so.* /usr/lib/ || true
+  mv opt/emby-server/extra/lib/libdrm.so.* /usr/lib/ || true
+  mv opt/emby-server/extra/lib/libmfx.so.* /usr/lib/ || true
+  mv opt/emby-server/extra/lib/libOpenCL.so.* /usr/lib/ || true
+}
+
+# If LOCAL_EMBY_DEB or check for version
+LOCAL_EMBY_DEB_FILES=(/tmp/emby*.deb)
+LOCAL_EMBY_DEB=""
+if (( ${#LOCAL_EMBY_DEB_FILES[@]} == 1 )); then
+  LOCAL_EMBY_DEB=LOCAL_EMBY_DEB_FILES[0]
 else
-  echo "Unknown architecture. Set the LINK environment variable to a URL of a .deb file from https://github.com/MediaBrowser/Emby.Releases/releases"
-  exit 1
+  if [[] ! -z "${LOCAL_EMBY_DEB:-}" && "$#" != 1 ]] ; then
+    echo "Usage: $0 EMBY_VERSION";
+    exit 1;
+  else
+    get_emby $1
+  fi
 fi
 
 BUILD_DIR=$(mktemp -d)
 pushd "${BUILD_DIR}"
-echo "Downloading Emby from ${LINK}"
-curl -L -o emby.deb ${LINK}
-ar x emby.deb data.tar.xz
-tar xf data.tar.xz
+LOCAL_EMBY_DEB=$(ls -1 /tmp/emby*.deb)
 
-
-# Put the ffmpeg binaries in the right place, ignoring missing files
-mv opt/emby-server/bin/ffmpeg /usr/bin/ffmpeg
-mv opt/emby-server/lib/libav*.so.* /usr/lib/ || true
-mv opt/emby-server/lib/libpostproc.so.* /usr/lib/ || true
-mv opt/emby-server/lib/libsw* /usr/lib/ || true
-mv opt/emby-server/extra/lib/libva*.so.* /usr/lib/ || true
-mv opt/emby-server/extra/lib/libdrm.so.* /usr/lib/ || true
-mv opt/emby-server/extra/lib/libmfx.so.* /usr/lib/ || true
-mv opt/emby-server/extra/lib/libOpenCL.so.* /usr/lib/ || true
+install_ffmpeg ${LOCAL_EMBY_DEB}
 
 popd
 rm -rf /tmp/*
